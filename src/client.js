e68718a40e08ddb419fd2919949019f09588647f
Paul  Pronko <propan@gmail.com>
2017-05-04 18:19:25 +0300

    Running Hello world with react "just" in 4 hours of config!

--- src/client.js
+++ src/client.js
@@ -1,3 +1,8 @@
 /**
  * Created by paul on 5/4/17.
  */
+import React      from 'react';
+import ReactDOM   from 'react-dom';
+import App        from 'components/App';
+
+ReactDOM.render(<App />, document.getElementById('react-view'));
