69b0673273b4b0c3eaf49897a827a1733a587619
Paul  Pronko <propan@gmail.com>
2017-05-16 18:23:02 +0300

    Bootstrap table added

--- src/components/TablePanel/TablePanel.jsx
+++ src/components/TablePanel/TablePanel.jsx
@@ -1,9 +1,31 @@
-import React, {Component} from "react";
+import React, {Component, PropTypes} from "react";
+import {connect} from "react-redux";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+import "./react-bootstrap-table.min.css";
 
 class TablePanel extends Component {
     render() {
-        return <div> Table stub </div>
+
+        return <div>
+            <BootstrapTable data={this.props.summary.summary} striped hover condensed>
+                <TableHeaderColumn isKey dataField='id' width='10%'>ID</TableHeaderColumn>
+                <TableHeaderColumn dataField='sugar_uname' width='40%'>Sugar Name</TableHeaderColumn>
+                <TableHeaderColumn dataField='team' width='20%'>Team</TableHeaderColumn>
+                <TableHeaderColumn dataField='timesheets_sum'>Hrs.</TableHeaderColumn>
+            </BootstrapTable>
+        </div>
     }
 }
 
-export default TablePanel;
+const mapStateToProps = state => {
+    return {
+        summary: state.summary
+    }
+};
+
+TablePanel.PropTypes = {
+    summary: PropTypes.node.isRequired
+};
+
+
+export default connect(mapStateToProps)(TablePanel);
